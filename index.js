d8d89a7f50916152d982fa4c11df310ef22a9de6
shah khilan <shahkhilan2901@gmail.com>
2026-02-21 21:23:33 +0530

    making get request

---
+++ index.js
@@ -0,0 +1,13 @@
+// app.js
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express with ES modules!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
